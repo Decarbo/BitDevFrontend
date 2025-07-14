094a94657cc95d2e8ba5f5841a3387732e3e88b9
Decarbo <decarbo77@gmail.com>
2025-07-14 12:11:54 +0530

    redux presist

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+import { Provider } from 'react-redux';
+import appstore, { persistor } from './store'; // 👈 import your store and persistor
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<Provider store={appstore}>
+			<PersistGate
+				loading={null}
+				persistor={persistor}>
+				<App />
+			</PersistGate>
+		</Provider>
+	</StrictMode>
+);
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,15 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage
+
+// Your reducers
 import userReducer from './userSlice';
 import feedReducer from './feedSlice';
 import connectionReducer from './connectionSlice';
 import requestReducer from './requestSlice';
-const appstore = configureStore({
-	reducer: {
-		user: userReducer,
-		feed: feedReducer,
-		connection: connectionReducer,
-		request: requestReducer,
-	},
+
+// Combine all reducers
+const rootReducer = combineReducers({
+	user: userReducer,
+	feed: feedReducer,
+	connection: connectionReducer,
+	request: requestReducer,
 });
 
+// Configure persist
+const persistConfig = {
+	key: 'root',
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create store
+const appstore = configureStore({
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
+});
+
+export const persistor = persistStore(appstore);
 export default appstore;
